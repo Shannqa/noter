f37ee72de9e21683127f50af1a61d860f0ff5de8
Shannqa <shannqa@gmail.com>
2026-07-25 21:30:32 +0200

    Add edit note

--- src/EditNote.jsx
+++ src/EditNote.jsx
@@ -1,16 +1,64 @@
 import { useState } from "react";
-import { useParams, useOutletContext } from "react-router";
+import { useParams, useOutletContext, useNavigate } from "react-router";
+import "./EditNote.css";
 
 function EditNote() {
-  const [allNotes] = useOutletContext();
+  const [allNotes, setAllNotes] = useOutletContext();
   const { id } = useParams();
   const note = allNotes.find((item) => item.id === id);
+  const [title, setTitle] = useState(note.title || "");
+  const [body, setBody] = useState(note.body || "");
+  const navigate = useNavigate();
+
+  function editNote() {
+    setAllNotes(
+      allNotes.map((item) => {
+        if (item.id == note.id) {
+          item.title = title;
+          item.body = body;
+        }
+        return item;
+      }),
+    );
+    localStorage.setItem(
+      "notes",
+      JSON.stringify(
+        allNotes.map((item) => {
+          if (item.id == note.id) {
+            item.title = title;
+            item.body = body;
+          }
+          return item;
+        }),
+      ),
+    );
+    return navigate("/note/" + id);
+  }
+
   if (!note) {
     return <h2>No note found</h2>;
   }
+
   return (
-    <div>
+    <div className="edit-note">
       <h2>Edit note</h2>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="note-title"
+      />
+      <textarea
+        placeholder="Note"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        autoFocus={true}
+        className="note-body"
+      ></textarea>
+      <button onClick={(e) => editNote()} className="submit-button">
+        Submit
+      </button>
     </div>
   );
 }
